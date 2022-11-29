6d06020033cf649b7a48f7f4b8261bea926ffdd7
Daiki Kusuyama (X1Y) <daiki.milky.kinako@gmail.com>
2022-11-29 15:32:09 +0900

    Squashed commit of the following:
    commit 7cc38ee1b068f47ad50a1943d7548ccb77c3d6eb
    Author: Daiki Kusuyama (UX490UAR) <daiki.milky.kinako@gmail.com>
    Date:   Sun Mar 20 19:13:14 2022 +0900
    
        subtle change in shell scriptwq
    
    commit 66a9e9f1d1c24a8e8826a1000d7ad7f4c8a214a9
    Author: Daiki Kusuyama (UX490UAR) <daiki.milky.kinako@gmail.com>
    Date:   Sun Mar 20 19:12:48 2022 +0900
    
        add sample
    
    commit 560a9586dc18827afeeee0929a27ef3909cd3d78
    Author: Daiki Kusuyama (UX490UAR) <daiki.milky.kinako@gmail.com>
    Date:   Sun Mar 20 17:22:20 2022 +0900
    
        initial commit

--- mktile.sh
+++ mktile.sh
@@ -14,14 +14,6 @@ if [ $fname == $fname_body ]; then
     exit 1
 fi
 
-<<<<<<< HEAD
-w=2 # 連番にする際の桁数
-startsec=6
-period=20
-fps=0.67 # 2/3fpsすなわち3秒で2枚の画像
-# トリミングする際に、全体の幅・高さから引くpixel数 (切り取る幅)
-minus_w=700
-=======
 rm -rf img/*
 
 w=2 # 連番にする際の桁数
@@ -30,7 +22,6 @@ period=17
 fps=0.8 # 1fps
 # トリミングする際に、全体の幅・高さから引くpixel数 (切り取る幅)
 minus_w=500
->>>>>>> 7cc38ee1b068f47ad50a1943d7548ccb77c3d6eb
 minus_h=0
 # 番号の描画を始める座標
 # 左上が0,0
@@ -40,19 +31,12 @@ ffmpeg -ss $startsec -t $period -i $fname -r $fps -vf crop=in_w-$minus_w:in_h-$m
 
 
 img_num=$(ls img/ | grep $fname_body | grep -v _converted.png | wc -l)
-<<<<<<< HEAD
-mode=1
-=======
 mode=0
->>>>>>> 7cc38ee1b068f47ad50a1943d7548ccb77c3d6eb
 # 左上に番号を振りたい時: 0
 # 右上に番号を振りたい時: 1
 # 右下に番号を振りたい時: 2
 # 左下に番号を振りたい時: 3
-<<<<<<< HEAD
-=======
 echo "./mkwatermark.py $fname_body $img_num $w $mode"
->>>>>>> 7cc38ee1b068f47ad50a1943d7548ccb77c3d6eb
 ./mkwatermark.py $fname_body $img_num $w $mode
 
 num_columns=5
